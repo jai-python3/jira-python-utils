373161757eadaa71db64948ae320518127c7e68c
Jay Sundaram <jaideep.sundaram@regeneron.com>
2023-02-19 16:21:15 -0500

    FIXES name of the Python executable script

--- jira_epics_to_confluence_tables.sh
+++ jira_epics_to_confluence_tables.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ${HOME}/.tools/jira-python-utils/venv/bin/activate
-python ${HOME}/.tools/jira-python-utils/jira_epics_to_confluence_table.py "$@"
+python ${HOME}/.tools/jira-python-utils/jira_epics_to_confluence_tables.py "$@"
