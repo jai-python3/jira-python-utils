cc2f371f1b2229eb987322dba7f89544c7e7a1ad
Jaideep Sundaram <jai.python3@gmail.com>
2024-03-07 09:59:48 -0500

    Bump version: 0.7.1 → 0.8.0

--- setup.py
+++ setup.py
@@ -75,6 +75,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jai-python3/jira-python-utils',
-    version='0.7.1',
+    version='0.8.0',
     zip_safe=False,
 )
